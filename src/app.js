94e290e8309ec0559086223f337e865cb87eadb5
Jesuspestana01 <jesuspestana77@gmail.com>
2021-10-29 00:17:30 -0400

    :rocket: initial commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,47 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let cards = {
+  topSymbol: ["&#9830", "&#9829", "&#9824", "&#9827"],
+  downSymbol: ["&#9830", "&#9829", "&#9824", "&#9827"],
+  cardNumbers: [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ]
 };
+
+let numbersAndSymbols = () => {
+  let allNumbers = Math.floor(Math.random() * cards.cardNumbers.length);
+  document.querySelector("#cardRandomNumber").innerHTML =
+    cards.cardNumbers[allNumbers];
+
+  let allSymbols = Math.floor(Math.random() * cards.topSymbol.length);
+  document.querySelector("#topSymbol").innerHTML = cards.topSymbol[allSymbols];
+  document.querySelector("#bottomSymbol").innerHTML =
+    cards.topSymbol[allSymbols];
+
+  if (
+    cards.topSymbol[allSymbols] === "&#9830" ||
+    cards.topSymbol[allSymbols] === "&#9829"
+  ) {
+    document.querySelector("#cardId").classList.add("symbolRed");
+  } else if (
+    cards.topSymbol[allSymbols] === "&#9827" ||
+    cards.topSymbol[allSymbols] === "&#9824"
+  ) {
+    document.querySelector("#cardId").classList.remove("symbolRed");
+  }
+};
+
+window.addEventListener("load", numbersAndSymbols);
